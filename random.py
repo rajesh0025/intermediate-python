dcc65be9d0e72757df9ba606485f4617ec04c4c5
rajesh0025 <70711676+rajesh0025@users.noreply.github.com>
2021-07-31 20:28:31 +0530

    Update random.py

--- random.py
+++ random.py
@@ -14,26 +14,26 @@ import random
 mylist=list("ABCDEFGH")
 
 a=random.choice(mylist)
-b=random.sample(mylist,3)#here set of any 3 nums of list here no multiple nums occurs
-c=random.choices(mylist,k=3)#same as of sample but multiple times ocurs a single value
+b=random.sample(mylist,3)#here set of any 3 nums of list here no multiple nums occurs.
+c=random.choices(mylist,k=3)#same as of sample but multiple times ocurs a single value.
 d=random.shuffle(mylist)
 print(a)
 print(b)
 print(c)
 print(d)
 
-#used to save that random value after running
+#used to save that random value after running.
 import random
-random.seed(1)#we need to give a num like index so that we may call it later
-a=random.uniform(1,10)#it will generate float random value
+random.seed(1)#we need to give a num like index so that we may call it later.
+a=random.uniform(1,10)#it will generate float random value.
 print(a)
-random.seed(1)#we need to give a num like index so that we may call it later
-a=random.uniform(1,10)#it will generate float random value
+random.seed(1)#we need to give a num like index so that we may call it later.
+a=random.uniform(1,10)#it will generate float random value.
 print(a)
 
 random.seed(2)
-a=random.uniform(1,10)#it will generate float random value
+a=random.uniform(1,10)# it will generate float random value.
 print(a)
 random.seed(2)
-a=random.uniform(1,10)#it will generate float random value
+a=random.uniform(1,10)#it will generate float random value.
 print(a)
