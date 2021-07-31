88a71f389acf1b98836fd303ca8d4a08d98dd4b3
rajesh0025 <70711676+rajesh0025@users.noreply.github.com>
2021-07-31 20:24:28 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,10 +1,10 @@
-#collection: counter,namedtuple,orderedDict,defaultdict,deque
+#collection : counter,namedtuple,orderedDict,defaultdict,deque
 from collections import Counter
 a ="aaaaaaaaaaaaabbbbbbbbbcccdddddd"
-my_counter=Counter(a)#counting each
+my_counter=Counter(a)#counting  each
 print(my_counter)
 print(my_counter.items())
 print(my_counter.keys())
 print(my_counter.values())
-print(my_counter.most_common(2))#first 2  
+print(my_counter.most_common(2))#first2  
 print(list(my_counter.elements()))
