a50de0c19daa908cefa213a837f7f960b1d33efd
rajesh0025 <70711676+rajesh0025@users.noreply.github.com>
2021-08-02 21:42:40 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,45 +1,46 @@
-# sets unordered mutable no duplicates 
+# sets unordered mutable no duplicates.
 myset={"hello"}
 print(myset)
 
 myset=set("hello")
 print(myset)
 
-setnum =set()#empty set must rpresent like this 
+setnum =set()#empty set must rpresent like this. 
 
 setnum.add(1)
 setnum.add(2)
 setnum.add(3)
 
-#we we use remove function then there would be an error coz 4 element was not in the set
+#we we use remove function then there would be an error coz 4 element was not in the set.
 
 setnum.discard(4)
 
-# .clear() empties set
+# .clear() empties set.
  
 print(setnum)
 
 
 setA ={1,2,3,4,5,6,7}
 setB={1,2,3,8,9,10,11}
-# union method to attach 2 sets
+
+# union method to attach 2 sets.
 attach=setA.union(setB)
 print(attach)
 
 
-#intersection to know common items of set A
+#intersection to know common items of set A.
 common=setA.intersection(setB)
 print(common)
 
-#difference of sets of setA
+#difference of sets of setA.
 difference=setA.difference(setB)
 print(difference)
 
-#differnt iteems in both sets
+#differnt iteems in both sets.
 difference=setA.symmetric_difference(setB)
 print(difference)
 
-#updating the original set 
+#updating the original set. 
 setA ={1,2,3,4,5,6,7}
 setB={1,2,3,8,9,10,11}
 
